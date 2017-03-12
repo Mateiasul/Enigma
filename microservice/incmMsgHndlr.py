a65b1e5cfa3b88ac52b03b5adcd053d82368a5f6
Andrei Vintila <andrei.vintila@student.manchester.ac.uk>
2017-03-12 05:00:26 +0000

    Reviewed and validated the code that contains the predefined messages. Also rephrased some questions. Made sure the text message replies match the voice replies in order to ensure consistency. Plus some code optimisation. It needs to be tested before being merged to the master branch!

--- microservice/incmMsgHndlr.py
+++ microservice/incmMsgHndlr.py
@@ -1,90 +1,153 @@
 from flask import Flask, request
 from twilio import twiml
- 
- 
+
+
 app = Flask(__name__)
 ok = 0
-mistakes = 0
+triesLeft = 0
+city = 0
+
+
 @app.route('/sms', methods=['POST'])
 def sms():
     global ok
-    global mistakes
+    global triesLeft
+    global city
     number = request.form['From']
     message_body = request.form['Body']
     message_body = message_body.lower()
 
-    question1 = """You will need to answer 4 related questions! First question: 
-                    You are standing in a place which was formally called Longacre Square,
-                    and that is also known as The Crossroads of the world. What is the name of this place?"""
-    question2 = "You are standing in front of the grand thing that holds a torch in its right hand. Can u guess its name?"
-    question3 = "You are watching a home Knicks game in an amazing stadium. Can u guess the name of this place?"
-    question4 = "You decided to take a walk in one of the 10th greatest parks in the world. Can u guess the name of this park?"
-    bravo = "Well done, you advanced to the next level!\n"
-    mistakeMsg = ""
-    if (mistakes == 2):
-    	mistakeMsg = "Incorrect answer. One more mistake and you lose! Try again!"
-    else:
-    	mistakeMsg = "Incorrect answer. You are still allowed to make " + str(2 - mistakes) + " more mistakes! Try again!"
-    lose = "Congratulations! You win!"
-    win = "You win!"
+    question1 = "You will need to answer 4 related questions! First question: "
+    question1 += "You are standing in a place which was formally called "
+    question1 += "Longacre Square, and that is also known as The Crossroads "
+    question1 += "of the world. What is the name of this place?"
 
-    
-    if (ok == 0):
-      if (message_body == "play"):
-    	  text = "Think of a city."
-      else:
-    	  text = "Invalid request!"
-    	  ok = -1
+    question2 = "You are standing in front of a colossal sculpture that "
+    question2 += "holds a torch in its right hand. What is its name?"
 
-    if (ok == 1):
-      if (message_body == "new york"):
-        text = question1
-      else:
-      	text = "This one will be added soon, meanwhile, think of a different city."
-      	ok -= 1
+    question3 = "You are watching a Knicks home game in an amazing stadium. "
+    question3 += "What is the name of the stadium?"
 
-    if (ok == 2):
-      if (message_body == "times square"):
-        text = bravo + question2
-      else:
-        text = mistakeMsg
-        mistakes += 1
-        ok -= 1
+    question4 = "You decided to take a walk in one of the 10 greatest parks "
+    question4 += "in the world. What is the name of this park?"
 
-    if (ok == 3):
-      if (message_body == "statue of liberty"):
-        text = bravo + question3
-      else:
-        text = mistakeMsg
-      	mistakes += 1
-        ok -= 1
+    questionp1 = "You will need to answer 4 related questions! "
+    questionp1 += "First question: During WW2, when Hitler visited Paris, "
+    questionp1 += "the French cut the lift cables on this monument so that "
+    questionp1 += "Hitler would have to climb the steps if he wanted to reach "
+    questionp1 += "the top. What is this significant monument?"
 
-    if (ok == 4):
-      if (message_body == "madison square garden"):
-        text = bravo + question4
-      else:
-        text = mistakeMsg
-      	mistakes += 1
-        ok -= 1
+    questionp2 = "Paris beat Barcelona 4-0 in their 2017 Champions League "
+    questionp2 += "home game in this stadium. What is it called?"
 
-    if (ok == 5):
-      if (message_body == "central park"):
-        text = bravo + win
-      else:
-        text = mistakeMsg
-      	mistakes += 1
-        ok -= 1
+    questionp3 = "The ashes of Joan of Arc were scattered here and this "
+    questionp3 += "river was the inspiration for lots of Impressionist "
+    questionp3 += "painters, such as Henri Matisse. "
+    questionp3 += "What is the name of this river?"
 
-    if (mistakes == 4):
-      text = lose
-      ok = -1
-      mistakes = 0
+    questionp4 = "What is the name of the biggest museum in the world, so "
+    questionp4 += "big that it is impossible to visit in only one day?"
 
-    ok += 1
+    bravo = "Well done, you advanced to the next level! "
+    bravo += "Now, to the next question: "
+
+    lose = "Sorry, you lost!"
+    win = "Congratulations! You win!"
+    mistakeMsg = "Incorrect answer. "
+
+    if ((triesLeft - 1) <= 0):
+        text = lose
+    elif ((triesLeft - 1) > 1):
+        mistakeMsg += "You are still allowed to make " + str(triesLeft - 1)
+        mistakeMsg += " more mistakes"
+    elif ((triesLeft - 1) == 1):
+        mistakeMsg += "You are still allowed to make one more mistake"
+    mistakeMsg += "! Try again!"
+
+    if (ok == 0 and city == 0):
+        if (message_body == "new york"):
+            city = 1
+            text = question1
+        elif (message_body == "paris"):
+            city = 2
+            text = questionp1
+        else:
+            text = "This one will be added soon, meanwhile, "
+            text += "think of a different city."
+            ok--
+
+    if (city == 1):
+        if (ok == 1):
+            if ((message_body == "times square" or
+                 message_body == "time square")):
+                text = question2
+            else:
+                triesLeft--
+                text = mistakeMsg
+                ok--
+        elif (ok == 2):
+            if ((message_body == "statue of liberty" or
+                 message_body == "the statue of liberty")):
+                text = bravo + question3
+            else:
+                triesLeft--
+                text = mistakeMsg
+                ok--
+        elif (ok == 3):
+            if (message_body == "madison square garden"):
+                text = bravo + question4
+            else:
+                triesLeft--
+                text = mistakeMsg
+                ok--
+        elif (ok == 4):
+            if (message_body == "central park"):
+                text = win
+            else:
+                triesLeft--
+                text = mistakeMsg
+                ok--
+
+    if (city == 2):
+        if (ok == 1):
+            if ((message_body == "eiffel tower" or
+                 message_body == "the eiffel tower")):
+                text = bravo + questionp2
+            else:
+                triesLeft--
+                text = mistakeMsg
+                ok--
+        elif (ok == 2):
+            if ((message_body == "stade de france" or
+                 message_body == "stadium of france")):
+                text = bravo + questionp3
+            else:
+                triesLeft--
+                text = mistakeMsg
+                ok--
+        elif (ok == 3):
+            if ((message_body == "the seine" or message_body == "seine" or
+                 message_body == "seine river" or
+                 message_body == "river seine")):
+                text = bravo + questionp4
+            else:
+                triesLeft--
+                text = mistakeMsg
+                ok--
+        elif (ok == 4):
+            if (message_body == "the louvre" or message_body == "louvre"):
+                text = win
+            else:
+                triesLeft--
+                text = mistakeMsg
+                ok--
+
+    ok++
 
     resp = twiml.Response()
     resp.message(text)
     return str(resp)
+
 
 if __name__ == '__main__':
     app.run()
--- src/index.js
+++ src/index.js
@@ -10,12 +10,16 @@ var helpText = "Say INTERACTIVE if you want to talk with Alexa or PLAY if you wo
 var helpTextAdd = "Enigma is a quiz that gives you interesting information about some great places around the world.";
 var helpNameText = "To who I should send the message?";
 var helpMessageText = "What message I should send?";
+
 var ok = 0;
 var triesLeft = 3;
 var city = 0;
+
+
 var TM = function () {
     AlexaSkill.call(this, APP_ID);
 };
+
 
 // Extend AlexaSkill
 TM.prototype = Object.create(AlexaSkill.prototype);
@@ -25,6 +29,7 @@ TM.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, s
     console.log("Session Started");
 };
 
+
 TM.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
     var speechOutput = "Welcome, you have started Enigma" + helpText; //Start text
     var repromptText = helpTextAdd;
@@ -33,9 +38,11 @@ TM.prototype.eventHandlers.onLaunch = function (launchRequest, session, response
     
 };
 
+
 TM.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
     console.log("Session Closed");
 };
+
 
 TM.prototype.intentHandlers = {
 	"EnigmaIntent": function (intent, session, response) {
@@ -88,36 +95,43 @@ TM.prototype.intentHandlers = {
         var question1 = "You will need to answer 4 related questions! First question: "
                         + "You are standing in a place which was formally called Longacre Square, "
                         + "and that is also known as The Crossroads of the world. What is the name of this place?";
-        var question2 = "You are standing in front of the grand thing that holds a torch in its right hand. What is its name?";
+        var question2 = "You are standing in front of a colossal sculpture that holds a torch in its right hand. What is its name?";
         var question3 = "You are watching a Knicks home game in an amazing stadium. What is the name of the stadium?";
         var question4 = "You decided to take a walk in one of the 10 greatest parks in the world. What is the name of this park?";
-        var questionp1 = "During WW2, when Hitler visited Paris, the French cut the lift cables on this monument so that Hitler would "
-                             + "have to climb the steps if he wanted to reach the top ."
-                             + " What is this significant monument?";
+        
+        var questionp1 = "You will need to answer 4 related questions! First question: "
+                         + "During WW2, when Hitler visited Paris, the French cut the lift cables on this monument so that Hitler would "
+                         + "have to climb the steps if he wanted to reach the top. What is this significant monument?";
+        var questionp2 = "Paris beat Barcelona 4-0 in their 2017 Champions League home game in this stadium. What is it called?";
+        var questionp3 = "The ashes of Joan of Arc were scattered here and this river was the inspiration "
+                         + "for lots of Impressionist painters, such as Henri Matisse. What is the name of this river?";
+        var questionp4 = "What is the name of the biggest museum in the world, so big that it is impossible to visit in only one day?";
+
         var bravo = "Well done, you advanced to the next level! Now, to the next question: ";
         var lose = "Sorry, you lost!";
-        var mistakeMsg = "Incorrect answer. ";
-        var win = "Congratulations! You win!";
+        var win = "Congratulations! You win! If you want to play again, say a new city!";
         var idleMsg = "Are you still there?";
-        if((triesLeft-1) <= 0)
-        {
+
+        var mistakeMsg = "Incorrect answer. ";
+        if ((triesLeft - 1) <= 0) {
             response.tell(lose);
         }
-        else if(triesLeft-1 > 1)
-        {
+        else if ((triesLeft - 1) > 1) {
             mistakeMsg += "You are still allowed to make " + (triesLeft - 1) + " more mistakes";
         }
-        else if((triesLeft - 1) == 1)
-        {
+        else if ((triesLeft - 1) == 1) {
             mistakeMsg += "You are still allowed to make one more mistake";
         }
+        mistakeMsg += "! Try again!";
+
         if (ok === 0 && city === 0)
         {
-            if (text == "new york") {
+            if (text == "new york")
+            {
                 city = 1;
                 response.ask(question1, idleMsg);
             }
-            else if(text == "paris")
+            else if (text == "paris")
             {   city = 2;
                 response.ask(questionp1, idleMsg);
             }
@@ -126,8 +140,9 @@ TM.prototype.intentHandlers = {
                 response.ask("This one will be added soon, meanwhile, think of a different city.", idleMsg);
                 ok--;
             }
-        }    
-        if(city == 1)
+        }  
+
+        if (city == 1)
         {
             if (ok == 1)   
             {
@@ -137,25 +152,19 @@ TM.prototype.intentHandlers = {
                 else
                 {  
                     triesLeft--;
-                    
-                    response.ask(mistakeMsg);
-                    
+                    response.ask(mistakeMsg, idleMsg);
                     ok--;
-                    
-            
                 }
             }
             else if (ok == 2) 
             {
-                if (text == "statue of liberty") {
+                if (text == "statue of liberty" || text == "the statue of liberty") {
                     response.ask(bravo + question3, idleMsg);
                 }
                 else
                 {
-                    
-                    
                     triesLeft--;
-                    response.ask(mistakeMsg);
+                    response.ask(mistakeMsg, idleMsg);
                     ok--;
                 }
             }
@@ -167,103 +176,79 @@ TM.prototype.intentHandlers = {
                 else
                 {
                     triesLeft--;
-                    response.ask(mistakeMsg);
+                    response.ask(mistakeMsg, idleMsg);
                     ok--;
-                    
                 }
             }
             else if (ok == 4)
             {
                 if (text == "central park") {
-                    response.ask(win + "If you want to play again say a new city!", idleMsg);
-                    
+                    response.ask(win, idleMsg);
                 }
                 else
                 {
                     triesLeft--;
-                    response.ask(mistakeMsg);
+                    response.ask(mistakeMsg, idleMsg);
                     ok--;
                 }
             }
-        
         }
-        if(city == 2)
+
+        if (city == 2)
         {
             if (ok == 1)
             {
                 if (text == "eiffel tower" || text == "the eiffel tower")
-                    response.ask("You are right!!! Next question. Paris beat Barcelona 4-0 in their 2017 Champions League home game in this stadium. What is it called?", "Are you still there?");
+                    response.ask(bravo + questionp2, idleMsg);
                 else
                 {  
                     triesLeft--;
-                    response.ask(mistakeMsg);
+                    response.ask(mistakeMsg, idleMsg);
                     ok--;
                 }
             }
             else if (ok == 2)
             {
-                if (text == "stade de france" || text == "stadium of france")
-                    response.ask("You are right again!!! Here's another. The ashes of Joan of Arc were scattered here and this river was the inspiration for lots of Impressionist painters, such as Henri Matisse. What is the name of this river?", "Are you still there?");
+                if (text == "stade de france" || text == "stadium of france") {
+                    response.ask(bravo + questionp3, idleMsg);
+                }
                 else
                 {
                     triesLeft--;
-                    response.ask(mistakeMsg);
+                    response.ask(mistakeMsg, idleMsg);
                     ok--;
                 }
             }
             else if (ok == 3)
             {
-                if(text == "the seine" || text == "seine" || text == "seine river" || text == "river seine")
-                {
-                    response.ask("Right again !!! One more. What's the name of the biggest museum in the world, so big that it's impossible to visit the whole museum in one day?");
+                if(text == "the seine" || text == "seine" || text == "seine river" || text == "river seine") {
+                    response.ask(bravo + questionp4, idleMsg);
                 }
                 else
                 {
                     triesLeft--;
-                    response.ask(mistakeMsg);
+                    response.ask(mistakeMsg, idleMsg);
                     ok--;
                 }
             }
             else if (ok == 4)
             {
-                if (text == "the louvre" || text == "louvre")
-                {
-                    response.tell(win, idleMsg);
+                if (text == "the louvre" || text == "louvre") {
+                    response.tell(win);
                 }
                 else
                 {
                     triesLeft--;
-                    response.ask(mistakeMsg);
+                    response.ask(mistakeMsg, idleMsg);
                     ok--;
                 }
             }
         }
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         ok++;
     }
 };
+
 
 // Create the handler that responds to the Alexa Request.
 exports.handler = function (event, context) {
